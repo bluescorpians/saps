34b537581bb5db5030965bc23837b6d36e740f50
sumit <sumit@DESKTOP-0SRUEI4>
2024-09-12 20:34:34 +0530

    Integration completed

--- banking/src/main/java/saps/banking/controller.java
+++ banking/src/main/java/saps/banking/controller.java
@@ -1,5 +1,15 @@
 package saps.banking;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class controller {
 
+	@RequestMapping("/")
+	@ResponseBody
+	public String index() {
+		return "Integration completed";
+	}
 }
